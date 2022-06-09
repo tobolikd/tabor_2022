6a5176201eb2543e2636447f0aefb4ec98b5e41c
David Tobolik <tobolikg@gmail.com>
2022-06-09 16:53:25 +0200

    Makefile and run script improvements

--- plizicka/log_generator/log_generator.sh
+++ plizicka/log_generator/log_generator.sh
@@ -5,6 +5,19 @@ NO_COLOR='\033[0m'        # Text Reset
 GREEN='\033[0;32m'        # Green
 YELLOW='\033[0;33m'       # Yellow
 
+# exitcodes help
+print_exitcodes()
+{
+    echo "Something went wrong, check the exitcode to find out what happened"
+    echo ""
+    echo "EXIT CODES:"
+    echo "0 - everything allright"
+    echo "1 - compilation fail"
+    echo "2 - mode change fail"
+    echo "3 - program run fail"
+    echo "4 - compiling .tex files failed (check ./output/*.log)"
+    return 0
+}
 
 # compile project
 printf "\n${GREEN}Compiling project${NO_COLOR}\n"
@@ -12,7 +25,7 @@ make || (print_exitcodes && exit 1)
 printf "${GREEN}DONE${NO_COLOR}\n"
 
 
-printf "\n${GREEN}Cleaning output dir${NO_COLOR}\n"
+printf "\n${GREEN}Wiping output dir${NO_COLOR}\n"
 # destroy everything living
 cd ./output
 make purge
@@ -31,17 +44,9 @@ printf "${GREEN}DONE${NO_COLOR}\n"
 printf "\n${GREEN}Compiling tex files in ${YELLOW}./output/${NO_COLOR}\n{\n\n"
 cd ./output
 make || (print_exitcodes && exit 4)
-# clean
-make clean
 printf "\n}${GREEN}DONE${NO_COLOR}\n"
 
-print_exitcodes()
-{
-    echo "Something went wrong, check the exitcode to find out what happened"
-    echo ""
-    echo "EXIT CODES:"
-    echo "1 - compilation fail"
-    echo "2 - mode change fail"
-    echo "3 - program run fail"
-    echo "4 - compiling .tex files failed (check ./output/*.log)"
-}
+# clean
+printf "\n${GREEN}Cleaning ${YELLOW}./output/${NO_COLOR}\n{\n\n"
+make clean
+printf "${GREEN}DONE${NO_COLOR}\n"
