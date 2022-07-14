5b17c3ee2249e8d2f3d7b6c5c6acb57735b8a0b0
David Tobolík <tobolikg@gmail.com>
2022-07-14 22:42:45 +0200

    feat: small changes

--- sestrelovani/codes_generator.cpp
+++ sestrelovani/codes_generator.cpp
@@ -9,42 +9,38 @@ int checkparse(char *string)
 {
     for (uint8_t i = 0; string[i] != '\0'; i++)
     {
-        std::cerr << "Checking " << string[i] << "\n";
         if (string[i] < '0' || string[i] > '9')
         {
-            std::cerr << string[i] << "is invalid";
             return 1;
         }
     }
-
-    std::cerr << "stop\n";
-
     return 0;
 }
 
 char* getcode(uint8_t nums)
 {
     char* code = (char *) malloc(sizeof(char) * 4);
+    code[3] = '\0';
     uint8_t idx;
 
     for (uint8_t i = 0; i < 3; i++)
-        code[i] = rand() % ('Z' - 'A') + 'A';
+        code[i] = rand() % ('Z' - 'A' + 1) + 'A';
 
     switch (nums)
     {
     case 1:
-        code[rand() % 3] = rand() % ('9' - '0') + '0';
+        code[rand() % 3] = rand() % ('9' - '0' + 1) + '0';
         break;
 
     case 2:
         idx = rand() % 3;
-        code[idx] = rand() % ('9' - '0') + '0';
-        code[(idx + rand() % 2 + 1) % 3] = rand() % ('9' - '0') + '0';
+        code[idx] = rand() % ('9' - '0' + 1) + '0';
+        code[(idx + rand() % 2 + 1) % 3] = rand() % ('9' - '0' + 1) + '0';
         break;
     
     case 3:
         for (uint8_t i = 0; i < 3; i++)
-            code[rand() % 3] = rand() % ('9' - '0') + '0';
+            code[rand() % 3] = rand() % ('9' - '0' + 1) + '0';
         break;
     
     default:
